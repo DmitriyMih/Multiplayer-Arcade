9ee5ccaf9dcb41fbd282a5104cdf34b2a5d8e303
Jew <dima.com655@gmail.com>
2022-10-01 19:19:45 +0300

    Big Fix

--- Assets/Models/GameSettings.cs
+++ Assets/Models/GameSettings.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class GameSettings : MonoBehaviour
 {
-    [SerializeField] private GameObject joysticksGroup;
-
     [SerializeField] private GameObject content;
     [SerializeField] private GameObject interactButton;
 
@@ -18,20 +16,21 @@ public class GameSettings : MonoBehaviour
 
     [SerializeField] private Toggle joystickToggle;
 
-    private List<Joystick> joysticks = new List<Joystick>();
+    [SerializeField] private List<Joystick> joysticks = new List<Joystick>();
 
     private void Awake()
     {
         content.SetActive(false);
         closedButton.SetActive(false);
-        Initialization();    
+        Initialization();
     }
 
     private void Initialization()
     {
-        joysticks.AddRange(joysticksGroup.GetComponentsInChildren<Joystick>());
         if (playerMovement != null)
             SwitchJoystick(joystickToggle.isOn);
+
+        SwitchControl(true);
     }
 
     public void SwitchControl(bool isButton)
@@ -40,10 +39,16 @@ public class GameSettings : MonoBehaviour
         {
             case true:
                 interactButton.SetActive(true);
+
+                lightImage[0].gameObject.SetActive(true);
+                lightImage[1].gameObject.SetActive(false);
                 break;
 
             case false:
                 interactButton.SetActive(false);
+
+                lightImage[0].gameObject.SetActive(false);
+                lightImage[1].gameObject.SetActive(true);
                 break;
         }
         playerMovement.ButtonInteractable = isButton;
@@ -58,18 +63,12 @@ public class GameSettings : MonoBehaviour
                 id = 0;
                 joysticks[0].gameObject.SetActive(true);
                 joysticks[1].gameObject.SetActive(false);
-
-                lightImage[0].gameObject.SetActive(true);
-                lightImage[1].gameObject.SetActive(false);
                 break;
 
             case false:
                 id = 1;
                 joysticks[0].gameObject.SetActive(false);
                 joysticks[1].gameObject.SetActive(true);
-
-                lightImage[0].gameObject.SetActive(false);
-                lightImage[1].gameObject.SetActive(true);
                 break;
         }
 
