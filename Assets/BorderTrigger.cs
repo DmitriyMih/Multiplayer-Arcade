c1f10aa738e0775d6b883644748b733e249171b2
Jew <dima.com655@gmail.com>
2022-10-01 19:57:27 +0300

    Fix

--- Assets/BorderTrigger.cs
+++ Assets/BorderTrigger.cs
@@ -6,6 +6,12 @@ public class BorderTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.position = new Vector3(0, 15, 0);
+        other.gameObject.transform.position = new Vector3(0, 15, 0);
+        Debug.Log($"|Enter| Object {other.gameObject} Teleportating To Spawn");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        other.gameObject.transform.position = new Vector3(0, 15, 0);
+        Debug.Log($"|Stay| Object {other.gameObject} Teleportating To Spawn");
     }
 }
